5f6119db7eb7042197696d23f0a02604fd23173b
Daeyun <sweet_ray@naver.com>
2026-08-23 22:12:30 +0900

    Chore: Firebase 서드파티 및 plist 파일 추가

--- FinQ/App/FinQApp.swift
+++ FinQ/App/FinQApp.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 @main
 struct FinQApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
