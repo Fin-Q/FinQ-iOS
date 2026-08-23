a63f2dd551612d9f52ef9e5be5fd337f51cda328
Daeyun <sweet_ray@naver.com>
2026-08-23 20:13:04 +0900

    Chore: 프로젝트 생성

---
+++ FinQ/FinQApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinQApp.swift
+//  FinQ
+//
+//  Created by 권대윤 on 8/23/26.
+//
+
+import SwiftUI
+
+@main
+struct FinQApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FinQTests/FinQTests.swift
@@ -0,0 +1,19 @@
+//
+//  FinQTests.swift
+//  FinQTests
+//
+//  Created by 권대윤 on 8/23/26.
+//
+
+import Testing
+@testable import FinQ
+
+struct FinQTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
